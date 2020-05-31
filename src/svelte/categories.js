f2633b8df090b0a04201d4a3d8645c3222043b07
Nolan Lawson <nolan@nolanlawson.com>
2020-05-31 12:56:44 -0700

    fix: tweak styles

--- src/svelte/categories.js
+++ src/svelte/categories.js
@@ -1,12 +1,12 @@
 // via https://unpkg.com/browse/emojibase-data@5.0.1/meta/groups.json
 export const categories = [
-  { group: 0, name: 'smileys-emotion' },
-  { group: 1, name: 'people-body' },
-  { group: 3, name: 'animals-nature' },
-  { group: 4, name: 'food-drink' },
-  { group: 5, name: 'travel-places' },
-  { group: 6, name: 'activities' },
-  { group: 7, name: 'objects' },
-  { group: 8, name: 'symbols' },
-  { group: 9, name: 'flags' }
+  { group: 0, emoji: '😀', name: 'smileys-emotion' },
+  { group: 1, emoji: '👋', name: 'people-body' },
+  { group: 3, emoji: '🐱', name: 'animals-nature' },
+  { group: 4, emoji: '🍎', name: 'food-drink' },
+  { group: 5, emoji: '🚗', name: 'travel-places' },
+  { group: 6, emoji: '⚽', name: 'activities' },
+  { group: 7, emoji: '✏', name: 'objects' },
+  { group: 8, emoji: '✅', name: 'symbols' },
+  { group: 9, emoji: '🏁', name: 'flags' }
 ]
