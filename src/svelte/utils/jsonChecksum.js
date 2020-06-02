0c60360a6ba91fe650a5b4c3afbbda8303800e09
Nolan Lawson <nolan@nolanlawson.com>
2020-06-01 18:57:46 -0700

    fix: simpler checksum

--- src/svelte/utils/jsonChecksum.js
+++ src/svelte/utils/jsonChecksum.js
@@ -1,12 +1,11 @@
 import stringify from 'fast-json-stable-stringify'
-import { binaryStringToArrayBuffer, arrayBufferToBlob, blobToBase64String } from 'blob-util'
+import { binaryStringToArrayBuffer, arrayBufferToBinaryString } from 'blob-util'
 
 // generate a checksum based on a stable stringify of a JSON object
 export async function jsonChecksum (object) {
   const string = stringify(object)
   const buffer = binaryStringToArrayBuffer(string)
   const outBuffer = await crypto.subtle.digest('SHA-1', buffer)
-  const outBlob = arrayBufferToBlob(outBuffer)
-  const outBase64 = await blobToBase64String(outBlob)
-  return outBase64
+  const outBinString = arrayBufferToBinaryString(outBuffer)
+  return btoa(outBinString)
 }
