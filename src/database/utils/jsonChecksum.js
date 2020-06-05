49ba3d25c276ad3ec7d083ed7a9ba2b39b4ddc5d
Nolan Lawson <nolan@nolanlawson.com>
2020-06-05 08:56:33 -0700

    fix: add  more perf marks

--- src/database/utils/jsonChecksum.js
+++ src/database/utils/jsonChecksum.js
@@ -1,11 +1,15 @@
 import stringify from 'fast-json-stable-stringify'
 import { binaryStringToArrayBuffer, arrayBufferToBinaryString } from 'blob-util'
+import { mark, stop } from '../../shared/marks'
 
 // generate a checksum based on a stable stringify of a JSON object
 export async function jsonChecksum (object) {
+  mark('jsonChecksum')
   const string = stringify(object)
   const buffer = binaryStringToArrayBuffer(string)
   const outBuffer = await crypto.subtle.digest('SHA-1', buffer)
   const outBinString = arrayBufferToBinaryString(outBuffer)
-  return btoa(outBinString)
+  const res = btoa(outBinString)
+  stop('jsonChecksum')
+  return res
 }
