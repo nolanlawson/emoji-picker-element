5c98bf65ff88e379cf7ba9628e18fb5f98c9d4fe
Nolan Lawson <nolan@nolanlawson.com>
2020-06-07 17:55:50 -0700

    test: add adhoc test

---
+++ test/adhoc/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import fs from 'fs'
+const app = express()
+const port = 3000
+
+app.use(express.static('./'))
+
+app.get('/', (req, res) => {
+  res.type('text/html')
+    .send(fs.readFileSync('./test/adhoc/index.html', 'utf8'))
+})
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
