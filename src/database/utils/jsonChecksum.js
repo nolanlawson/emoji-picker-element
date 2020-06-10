021fa60564e927f29ead9671d3ee8b450ce62369
Nolan Lawson <nolan@nolanlawson.com>
2020-06-10 07:59:13 -0700

    refactor: refactor code and comments

--- src/database/utils/jsonChecksum.js
+++ src/database/utils/jsonChecksum.js
@@ -1,12 +1,12 @@
 import { binaryStringToArrayBuffer, arrayBufferToBinaryString } from 'blob-util'
 import { mark, stop } from '../../shared/marks'
 
-// generate a checksum based on a stable stringify of a JSON object
+// generate a checksum based on the stringified JSON
 export async function jsonChecksum (object) {
   mark('jsonChecksum')
-  const string = JSON.stringify(object)
-  const buffer = binaryStringToArrayBuffer(string)
-  const outBuffer = await crypto.subtle.digest('SHA-1', buffer)
+  const inString = JSON.stringify(object)
+  const inBuffer = binaryStringToArrayBuffer(inString)
+  const outBuffer = await crypto.subtle.digest('SHA-1', inBuffer)
   const outBinString = arrayBufferToBinaryString(outBuffer)
   const res = btoa(outBinString)
   stop('jsonChecksum')
