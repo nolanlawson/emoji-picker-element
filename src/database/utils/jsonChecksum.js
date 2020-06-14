00d35c6b3ef0daaedb45989f71bcfb64f2e05ae6
Nolan Lawson <nolan@nolanlawson.com>
2020-06-13 18:05:35 -0700

    refactor: add some code comments

--- src/database/utils/jsonChecksum.js
+++ src/database/utils/jsonChecksum.js
@@ -6,6 +6,7 @@ export async function jsonChecksum (object) {
   mark('jsonChecksum')
   const inString = JSON.stringify(object)
   const inBuffer = binaryStringToArrayBuffer(inString)
+  // this does not need to be cryptographically secure, SHA-1 is fine
   const outBuffer = await crypto.subtle.digest('SHA-1', inBuffer)
   const outBinString = arrayBufferToBinaryString(outBuffer)
   const res = btoa(outBinString)
