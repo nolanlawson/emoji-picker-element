297d4ad9e873c34f5134799bd532ed9f9fa073b3
Nolan Lawson <nolan@nolanlawson.com>
2020-06-16 23:12:42 -0700

    perf: remove logging code from prod

--- src/picker/utils/emojiSupport.js
+++ src/picker/utils/emojiSupport.js
@@ -2,15 +2,17 @@ import { determineEmojiSupportLevel } from './determineEmojiSupportLevel'
 import { log } from '../../shared/log'
 import { requestIdleCallback } from './requestIdleCallback'
 // Check which emojis we know for sure aren't supported, based on Unicode version level
-export const emojiSupportLevelPromise = new Promise(resolve => {
-  requestIdleCallback(() => {
+export const emojiSupportLevelPromise = new Promise(resolve => (
+  requestIdleCallback(() => (
     resolve(determineEmojiSupportLevel()) // delay so ideally this can run while IDB is first populating
-  })
-})
+  ))
+))
 // determine which emojis containing ZWJ (zero width joiner) characters
 // are supported (rendered as one glyph) rather than unsupported (rendered as two or more glyphs)
 export const supportedZwjEmojis = new Map()
 
-emojiSupportLevelPromise.then(emojiSupportLevel => {
-  log('emoji support level', emojiSupportLevel)
-})
+if (process.env.NODE_ENV !== 'production') {
+  emojiSupportLevelPromise.then(emojiSupportLevel => {
+    log('emoji support level', emojiSupportLevel)
+  })
+}
