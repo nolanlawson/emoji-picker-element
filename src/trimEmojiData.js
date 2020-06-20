98acb9be3c8511d5e4999820d3fa68da03e60cec
Nolan Lawson <nolan@nolanlawson.com>
2020-06-20 15:07:59 -0700

    fix: improve trimEmojiData

--- src/trimEmojiData.js
+++ src/trimEmojiData.js
@@ -4,13 +4,25 @@ import { requiredKeys } from './database/utils/requiredKeys'
 const optionalKeys = ['skins', 'emoticon']
 const allKeys = [...requiredKeys, ...optionalKeys]
 
+const allSkinsKeys = ['tone', 'emoji', 'version']
+
 export default function trimEmojiData (emojiBaseData) {
   assertEmojiBaseData(emojiBaseData)
   return emojiBaseData.map(emoji => {
     const res = {}
     for (const key of allKeys) {
       if (key in emoji) {
-        res[key] = emoji[key]
+        if (key === 'skins') { // trim skins even further
+          res[key] = emoji[key].map(skin => {
+            const skinRes = {}
+            for (const skinKey of allSkinsKeys) {
+              skinRes[skinKey] = skin[skinKey]
+            }
+            return skinRes
+          })
+        } else {
+          res[key] = emoji[key]
+        }
       }
     }
     return res
