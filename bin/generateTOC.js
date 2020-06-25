2b2aff30cb3a29c46409211867aac1464ff77bb6
Nolan Lawson <nolan@nolanlawson.com>
2020-06-24 18:01:52 -0700

    chore: fix travis badge in TOC

--- bin/generateTOC.js
+++ bin/generateTOC.js
@@ -5,10 +5,13 @@ import { replaceInReadme } from './replaceInReadme.js'
 const TOC_START_MARKER = '<!-- toc start -->'
 const TOC_END_MARKER = '<!-- toc end -->'
 
+const TRAVIS_BADGE = /\[!\[Build Status]\(.*?\)]\(.*?\)/
+
 async function main () {
-  const content = await readFile('./README.md', 'utf8')
-  const toc = markdownToc(content)
-  await replaceInReadme(TOC_START_MARKER, TOC_END_MARKER, toc.content)
+  const content = (await readFile('./README.md', 'utf8'))
+    .replace(TRAVIS_BADGE, '') // remove travis badge, it confuses markdown-toc
+  const toc = markdownToc(content).content
+  await replaceInReadme(TOC_START_MARKER, TOC_END_MARKER, toc)
 }
 
 main().catch(err => {
