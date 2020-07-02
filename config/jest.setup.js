414f0a6a80bdbfc0289b4d9949f1555f6b15a114
Nolan Lawson <nolan@nolanlawson.com>
2020-07-02 10:05:52 -0700

    test: use fresh IDB for every test

--- config/jest.setup.js
+++ config/jest.setup.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom/extend-expect'
-import 'fake-indexeddb/auto'
+import FDBFactory from 'fake-indexeddb/build/FDBFactory'
+import FDBKeyRange from 'fake-indexeddb/build/FDBKeyRange'
 import { Crypto } from '@peculiar/webcrypto'
 
 jest.mock('node-fetch', () => require('fetch-mock-jest').sandbox())
@@ -10,3 +11,8 @@ global.Response = fetch.Response
 global.crypto = new Crypto()
 
 process.env.NODE_ENV = 'test'
+
+global.IDBKeyRange = FDBKeyRange
+beforeEach(() => {
+  global.indexedDB = new FDBFactory() // fresh indexedDB for every test
+})
