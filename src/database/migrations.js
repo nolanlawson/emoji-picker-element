815ff2c80880fb8afefa703d302ddee6f833bea1
Nolan Lawson <nolan@nolanlawson.com>
2020-08-16 15:19:46 -0700

    reuse more old migration code to reduce risk

--- src/database/migrations.js
+++ src/database/migrations.js
@@ -8,14 +8,28 @@ import {
   INDEX_TOKENS, INDEX_COUNT, INDEX_SKIN_UNICODE, FIELD_SKIN_UNICODE
 } from './constants'
 
-export function initialMigration (db) {
-  db.createObjectStore(STORE_KEYVALUE)
+function initialMigration (db) {
+  function createObjectStore (name, keyPath, indexes) {
+    const store = keyPath
+      ? db.createObjectStore(name, { keyPath })
+      : db.createObjectStore(name)
+    if (indexes) {
+      for (const [indexName, [keyPath, multiEntry]] of Object.entries(indexes)) {
+        store.createIndex(indexName, keyPath, { multiEntry })
+      }
+    }
+    return store
+  }
 
-  const emojiStore = db.createObjectStore(STORE_EMOJI, { keyPath: FIELD_UNICODE })
-  emojiStore.createIndex(INDEX_TOKENS, FIELD_TOKENS, { multiEntry: true })
-  emojiStore.createIndex(INDEX_GROUP_AND_ORDER, [FIELD_GROUP, FIELD_ORDER])
-  emojiStore.createIndex(INDEX_SKIN_UNICODE, FIELD_SKIN_UNICODE, { multiEntry: true })
-
-  const favoritesStore = db.createObjectStore(STORE_FAVORITES)
-  favoritesStore.createIndex(INDEX_COUNT, '')
+  createObjectStore(STORE_KEYVALUE)
+  createObjectStore(STORE_EMOJI, /* keyPath */ FIELD_UNICODE, {
+    [INDEX_TOKENS]: [FIELD_TOKENS, /* multiEntry */ true],
+    [INDEX_GROUP_AND_ORDER]: [[FIELD_GROUP, FIELD_ORDER]],
+    [INDEX_SKIN_UNICODE]: [FIELD_SKIN_UNICODE, /* multiEntry */ true]
+  })
+  createObjectStore(STORE_FAVORITES, undefined, {
+    [INDEX_COUNT]: ['']
+  })
 }
+
+export { initialMigration }
