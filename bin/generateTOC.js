7c4cecd659bd5f1fec258e969e75b4a20c6df0e3
Nolan Lawson <nolan@nolanlawson.com>
2020-12-13 11:37:37 -0800

    docs: fix status badge in toc

--- bin/generateTOC.js
+++ bin/generateTOC.js
@@ -5,11 +5,11 @@ import { replaceInReadme } from './replaceInReadme.js'
 const TOC_START_MARKER = '<!-- toc start -->'
 const TOC_END_MARKER = '<!-- toc end -->'
 
-const TRAVIS_BADGE = /\[!\[Build Status]\(.*?\)]\(.*?\)/
+const CI_BADGE = /\[!\[Build status]\(.*?\)]\(.*?\)/
 
 async function main () {
   const content = (await readFile('./README.md', 'utf8'))
-    .replace(TRAVIS_BADGE, '') // remove travis badge, it confuses markdown-toc
+    .replace(CI_BADGE, '') // remove travis badge, it confuses markdown-toc
   const toc = markdownToc(content).content
     .replace('#emoji-picker-element', '#emoji-picker-element-') // it confuses GitHub's anchor links too
   await replaceInReadme(TOC_START_MARKER, TOC_END_MARKER, toc)
