6c337357c12425f916cc2197fbba3640805277a0
Nolan Lawson <nolan@nolanlawson.com>
2020-12-27 15:05:06 -0800

    test: improve coverage for skintone picker

--- test/spec/picker/shared.js
+++ test/spec/picker/shared.js
@@ -4,10 +4,10 @@ import {
 } from '@testing-library/dom'
 
 export const openSkintoneListbox = async (container, skipActiveElementCheck) => {
-  await waitFor(() => expect(getByRole(container, 'button', { name: 'Choose a skin tone (currently Default)' }))
+  await waitFor(() => expect(getByRole(container, 'button', { name: /Choose a skin tone/ }))
     .toBeVisible())
   expect(queryAllByRole(container, 'listbox', { name: 'Skin tones' })).toHaveLength(0)
-  await fireEvent.click(getByRole(container, 'button', { name: 'Choose a skin tone (currently Default)' }))
+  await fireEvent.click(getByRole(container, 'button', { name: /Choose a skin tone/ }))
   await waitFor(() => expect(getByRole(container, 'listbox', { name: 'Skin tones' })).toBeVisible())
   expect(getAllByRole(container, 'option')).toHaveLength(6)
   getByRole(container, 'option', { name: 'Default', selected: true }).focus()
