cc70e57ca6c1b1e06100f7589f3c69b09499d4f7
Nolan Lawson <nolan@nolanlawson.com>
2021-06-28 07:14:09 -0700

    fix: un-break test

--- src/picker/PickerElement.js
+++ src/picker/PickerElement.js
@@ -11,16 +11,16 @@ export default class Picker extends SveltePicker {
     super({ props })
   }
 
-  // disconnectedCallback () {
-  //   const runAll = funcs => (funcs && funcs.forEach(func => func()))
-  //   // For Svelte v <3.33.0, we have to run the destroy logic ourselves because it doesn't have this fix:
-  //   // https://github.com/sveltejs/svelte/commit/d4f98f
-  //   // We can safely just run on_disconnect and on_destroy to cover all versions of Svelte. In older versions
-  //   // the on_destroy array will have length 1, whereas in more recent versions it'll be on_disconnect instead.
-  //   // TODO: remove this when we drop support for Svelte < 3.33.0
-  //   runAll(this.$$.on_destroy)
-  //   runAll(this.$$.on_disconnect)
-  // }
+  disconnectedCallback () {
+    const runAll = funcs => (funcs && funcs.forEach(func => func()))
+    // For Svelte v <3.33.0, we have to run the destroy logic ourselves because it doesn't have this fix:
+    // https://github.com/sveltejs/svelte/commit/d4f98f
+    // We can safely just run on_disconnect and on_destroy to cover all versions of Svelte. In older versions
+    // the on_destroy array will have length 1, whereas in more recent versions it'll be on_disconnect instead.
+    // TODO: remove this when we drop support for Svelte < 3.33.0
+    runAll(this.$$.on_destroy)
+    runAll(this.$$.on_disconnect)
+  }
 
   static get observedAttributes () {
     return ['locale', 'data-source', 'skin-tone-emoji'] // complex objects aren't supported, also use kebab-case
