8a0c339c2d2dc5cff8b6995ebb54c24d5371ba19
Nolan Lawson <nolan@nolanlawson.com>
2021-07-14 20:09:05 -0700

    fix: remove semis

--- react.d.ts
+++ react.d.ts
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import { CustomEmoji, EmojiClickEvent, I18n, CategorySortingFunction, SkinToneChangeEvent } from './shared';
+import * as React from 'react'
+import { CustomEmoji, EmojiClickEvent, I18n, CategorySortingFunction, SkinToneChangeEvent } from './shared'
 
 export interface EmojiPickerProps {
-    className?: string;
-    style?: React.CSSProperties;
-    locale?: string;
-    dataSource?: string;
-    skinToneEmoji?: string;
-    customCategorySorting?: CategorySortingFunction;
-    customEmoji?: CustomEmoji;
-    i18n?: I18n;
-    onEmojiClick?: (event: EmojiClickEvent) => void;
-    onSkinToneChange?: (event: SkinToneChangeEvent) => void;
+    className?: string
+    style?: React.CSSProperties
+    locale?: string
+    dataSource?: string
+    skinToneEmoji?: string
+    customCategorySorting?: CategorySortingFunction
+    customEmoji?: CustomEmoji
+    i18n?: I18n
+    onEmojiClick?: (event: EmojiClickEvent) => void
+    onSkinToneChange?: (event: SkinToneChangeEvent) => void
 }
 
-declare const EmojiPicker: (props: EmojiPickerProps) => JSX.Element;
+declare const EmojiPicker: (props: EmojiPickerProps) => JSX.Element
 
 export default EmojiPicker
