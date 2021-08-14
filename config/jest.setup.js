780d5b3c025780d6faf1b275a7fa52b03e7b1485
Nolan Lawson <nolan@nolanlawson.com>
2021-08-13 18:09:40 -0700

    test: inject actual styles in jest (#214)
    * test: inject actual styles in jest
    
    * fix: remove rollup plugin virtual

--- config/jest.setup.js
+++ config/jest.setup.js
@@ -4,6 +4,7 @@ import FDBKeyRange from 'fake-indexeddb/build/FDBKeyRange'
 import { Crypto } from '@peculiar/webcrypto'
 import { ResizeObserver } from 'd2l-resize-aware/resize-observer-module.js'
 import { deleteDatabase } from '../src/database/databaseLifecycle'
+import styles from '../node_modules/.cache/emoji-picker-element/styles.js'
 
 if (!global.performance) {
   global.performance = {}
@@ -25,12 +26,10 @@ global.ResizeObserver = ResizeObserver
 
 process.env.NODE_ENV = 'test'
 
+process.env.STYLES = styles
+
 global.IDBKeyRange = FDBKeyRange
 global.indexedDB = new FDBFactory()
-
-// TODO: figure out how to get the styles into Jest. For now it doesn't really
-// matter because none of the tests rely on visibility checks etc.
-jest.mock('emoji-picker-element-styles', () => '', { virtual: true })
 
 beforeAll(() => {
   jest.spyOn(global.console, 'log').mockImplementation()
