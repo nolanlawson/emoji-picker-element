fa7390517e7075e6408e2ef21286f61373310420
Matt Toschlog <matt@toschlog.com>
2021-12-13 15:50:25 -0500

    Added ms_MY.js (Malaysian text)

---
+++ src/picker/i18n/ms_MY.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'Kategori',
+  emojiUnsupportedMessage: 'Penyemak imbas anda tidak menyokong emoji warna.',
+  favoritesLabel: 'Kegemaran',
+  loadingMessage: 'Memuat…',
+  networkErrorMessage: 'Tidak dapat memuatkan emoji.',
+  regionLabel: 'Pemilih emoji',
+  searchDescription: 'Apabila hasil carian tersedia, tekan atas atau bawah untuk memilih dan tekan masukkan untuk memilih.',
+  searchLabel: 'Cari',
+  searchResultsLabel: 'Hasil carian',
+  skinToneDescription: 'Apabila dikembangkan, tekan atas atau bawah untuk memilih dan tekan masukkan untuk memilih.',
+  skinToneLabel: 'Pilih warna kulit (pada masa ini {skinTone})',
+  skinTonesLabel: 'Warna kulit',
+  skinTones: [
+    'Lalai',
+    'Cerah',
+    'Kuning langsat',
+    'Sederhana cerah',
+    'Sawo matang',
+    'Gelap'
+  ],
+  categories: {
+    custom: 'Tersuai',
+    'smileys-emotion': 'Smiley dan emotikon',
+    'people-body': 'Orang dan badan',
+    'animals-nature': 'Haiwan dan alam semula jadi',
+    'food-drink': 'Makanan dan minuman',
+    'travel-places': 'Perjalanan dan tempat',
+    activities: 'Aktiviti',
+    objects: 'Objek',
+    symbols: 'Simbol',
+    flags: 'Bendera'
+  }
+}
