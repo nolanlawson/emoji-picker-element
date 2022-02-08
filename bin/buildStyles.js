0bd08aa86f108058719ee7756c64de35d70f7222
Nolan Lawson <nolan@nolanlawson.com>
2022-02-08 07:16:10 -0800

    fix: fixup

--- bin/buildStyles.js
+++ bin/buildStyles.js
@@ -1,8 +1,8 @@
 import sass from 'sass'
-import csso from 'csso'
+import { minify } from 'csso'
 
 export function buildStyles () {
   const file = './src/picker/styles/picker.scss'
   const css = sass.renderSync({ file, outputStyle: 'compressed' }).css.toString('utf8')
-  return csso.minify(css).css
+  return minify(css).css
 }
