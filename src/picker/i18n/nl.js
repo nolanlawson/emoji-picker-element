d7727a3d2e5b8f3b88cc9327c7467c1b1d87a148
Melvin Oostendorp <info@arbarwings.nl>
2022-03-13 03:36:51 +0900

    feat: added Dutch translation file (#267)
    * Added nl.js (Dutch text)
    
    * chore: fix lint
    
    Co-authored-by: Nolan Lawson <nolan@nolanlawson.com>

---
+++ src/picker/i18n/nl.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'Categorieën',
+  emojiUnsupportedMessage: 'Uw browser ondersteunt geen kleurenemoji.',
+  favoritesLabel: 'Favorieten',
+  loadingMessage: 'Bezig met laden…',
+  networkErrorMessage: 'Kan emoji niet laden.',
+  regionLabel: 'Emoji-kiezer',
+  searchDescription: 'Als er zoekresultaten beschikbaar zijn, drukt u op omhoog of omlaag om te selecteren en op enter om te kiezen.',
+  searchLabel: 'Zoeken',
+  searchResultsLabel: 'Zoekresultaten',
+  skinToneDescription: 'Wanneer uitgevouwen, druk omhoog of omlaag om te selecteren en enter om te kiezen.',
+  skinToneLabel: 'Kies een huidskleur (momenteel {skinTone})',
+  skinTonesLabel: 'Huidskleuren',
+  skinTones: [
+    'Standaard',
+    'Licht',
+    'Medium-Licht',
+    'Medium',
+    'Middeldonker',
+    'Donker'
+  ],
+  categories: {
+    custom: 'Aangepast',
+    'smileys-emotion': 'Smileys en emoticons',
+    'people-body': 'Mensen en lichaam',
+    'animals-nature': 'Dieren en natuur',
+    'food-drink': 'Eten en drinken',
+    'travel-places': 'Reizen en plaatsen',
+    activities: 'Activiteiten',
+    objects: 'Voorwerpen',
+    symbols: 'Symbolen',
+    flags: 'Vlaggen'
+  }
+}
