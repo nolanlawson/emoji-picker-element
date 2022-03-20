c45e3e9b4368d36716eaf325217b81c2c29905c5
Nolan Lawson <nolan@nolanlawson.com>
2022-03-20 14:55:23 -0700

    fix: newer emoji and flags on Firefox on Windows (#271)
    * fix: emoji v14 and flags on Firefox on Windows
    
    * fix: add comment

--- src/picker/constants.js
+++ src/picker/constants.js
@@ -23,8 +23,13 @@ export const MOST_COMMONLY_USED_EMOJI = [
   '😘'
 ]
 
-export const FONT_FAMILY = '"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol",' +
-  '"Twemoji Mozilla","Noto Color Emoji","EmojiOne Color","Android Emoji",sans-serif'
+// It's important to list Twemoji Mozilla before everything else, because Mozilla bundles their
+// own font on some platforms (notably Windows and Linux as of this writing). Typically Mozilla
+// updates faster than the underlying OS, and we don't want to render older emoji in one font and
+// newer emoji in another font:
+// https://github.com/nolanlawson/emoji-picker-element/pull/268#issuecomment-1073347283
+export const FONT_FAMILY = '"Twemoji Mozilla","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol",' +
+  '"Noto Color Emoji","EmojiOne Color","Android Emoji",sans-serif'
 
 /* istanbul ignore next */
 export const DEFAULT_CATEGORY_SORTING = (a, b) => a < b ? -1 : a > b ? 1 : 0
