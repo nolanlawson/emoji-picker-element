a92f13b82b0e6545712023b6ff58bf410b9982ff
Nolan Lawson <nolan@nolanlawson.com>
2022-03-20 14:23:29 -0700

    fix: add comment

--- src/picker/constants.js
+++ src/picker/constants.js
@@ -23,6 +23,11 @@ export const MOST_COMMONLY_USED_EMOJI = [
   '😘'
 ]
 
+// It's important to list Twemoji Mozilla before everything else, because Mozilla bundles their
+// own font on some platforms (notably Windows and Linux as of this writing). Typically Mozilla
+// updates faster than the underlying OS, and we don't want to render older emoji in one font and
+// newer emoji in another font:
+// https://github.com/nolanlawson/emoji-picker-element/pull/268#issuecomment-1073347283
 export const FONT_FAMILY = '"Twemoji Mozilla","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol",' +
   '"Noto Color Emoji","EmojiOne Color","Android Emoji",sans-serif'
 
