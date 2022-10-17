0607ac39b5f8573704179ad367f038091a6b83a5
Kronia <45751612+kroniapp@users.noreply.github.com>
2022-10-17 16:30:36 +0200

    feat: add italian translation file (#291)

---
+++ src/picker/i18n/it.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'Categorie',
+  emojiUnsupportedMessage: 'Il tuo browser non supporta le emoji colorate.',
+  favoritesLabel: 'Preferiti',
+  loadingMessage: 'Caricamento...',
+  networkErrorMessage: 'Impossibile caricare le emoji.',
+  regionLabel: 'Selezione emoji',
+  searchDescription: 'Quando i risultati della ricerca sono disponibili, premi su o giù per selezionare e invio per scegliere.',
+  searchLabel: 'Cerca',
+  searchResultsLabel: 'Risultati di ricerca',
+  skinToneDescription: 'Quando espanso, premi su o giù per selezionare e invio per scegliere.',
+  skinToneLabel: 'Scegli una tonalità della pelle (corrente {skinTone})',
+  skinTonesLabel: 'Tonalità della pelle',
+  skinTones: [
+    'Predefinita',
+    'Chiara',
+    'Medio-Chiara',
+    'Media',
+    'Medio-Scura',
+    'Scura'
+  ],
+  categories: {
+    custom: 'Personalizzata',
+    'smileys-emotion': 'Faccine ed emozioni',
+    'people-body': 'Persone e corpi',
+    'animals-nature': 'Animali e natura',
+    'food-drink': 'Cibi e bevande',
+    'travel-places': 'Viaggi e luoghi',
+    activities: 'Attività',
+    objects: 'Oggetti',
+    symbols: 'Simboli',
+    flags: 'Bandiere'
+  }
+}
