ea2dbe7054d7b370ce6ab074ad93b2e295f2226d
Nolan Lawson <nolan@nolanlawson.com>
2022-12-18 11:54:06 -0800

    fix: fix code coverage

--- src/shared/lang.js
+++ src/shared/lang.js
@@ -2,6 +2,7 @@
 export const EMPTY_ARRAY = []
 export const EMPTY_OBJECT = {}
 
+/* istanbul ignore else */
 if (process.env.NODE_ENV !== 'production') {
   // In dev/test mode, we want to know if we are accidentally mutating these. Throw an error on mutation.
   Object.freeze(EMPTY_ARRAY)
