4472924eeafd5f179743350972c7d0f7cb8eac9e
Nolan Lawson <nolan@nolanlawson.com>
2022-12-18 11:10:16 -0800

    fix: add comment

--- src/shared/lang.js
+++ src/shared/lang.js
@@ -3,6 +3,7 @@ export const EMPTY_ARRAY = []
 export const EMPTY_OBJECT = {}
 
 if (process.env.NODE_ENV !== 'production') {
+  // In dev/test mode, we want to know if we are accidentally mutating these. Throw an error on mutation.
   Object.freeze(EMPTY_ARRAY)
   Object.freeze(EMPTY_OBJECT)
 }
