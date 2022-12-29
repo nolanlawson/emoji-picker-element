209b7519f2a2db1dd53e5c95abcbf7e9459c0742
Nolan Lawson <nolan@nolanlawson.com>
2022-12-28 16:00:53 -0800

    fix: timing tweak

--- src/picker/utils/emojiSupport.js
+++ src/picker/utils/emojiSupport.js
@@ -1,14 +1,14 @@
 import { determineEmojiSupportLevel } from './determineEmojiSupportLevel'
-import { requestIdleCallback } from './requestIdleCallback'
 // Check which emojis we know for sure aren't supported, based on Unicode version level
 let promise
 export const detectEmojiSupportLevel = () => {
   if (!promise) {
-    promise = new Promise(resolve => (
-      requestIdleCallback(() => (
-        resolve(determineEmojiSupportLevel()) // delay so ideally this can run while IDB is first populating
-      ))
-    ))
+    console.log('Queueing microtask for detectEmojiSupportLevel (should occur after creating the database')
+    // Delay so it can run while the IDB database is being created by the browser (on another thread).
+    // In the user timings, you should see "createDatabase" happening concurrently with "determineEmojiSupportLevel".
+    promise = Promise.resolve().then(() => {
+      return determineEmojiSupportLevel()
+    })
 
     /* istanbul ignore else */
     if (process.env.NODE_ENV !== 'production') {
