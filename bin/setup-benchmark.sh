55d87a8465e359ea651a94d2c9d88c320cfb7031
Nolan Lawson <nolan@nolanlawson.com>
2023-01-22 09:06:37 -0800

    fix: update script

--- bin/setup-benchmark.sh
+++ bin/setup-benchmark.sh
@@ -4,11 +4,6 @@ set -e
 
 cd ./test/benchmark
 
-# fix Tachometer not finding the local dependency using a symlink hack
-rm -fr ./node_modules
-mkdir ./node_modules
-ln -s ../../.. ./node_modules/emoji-picker-element
-
 # have Tachometer host the data.json right here
 rm -f ./data.json
 ln -s ../../node_modules/emoji-picker-element-data/en/emojibase/data.json ./data.json
