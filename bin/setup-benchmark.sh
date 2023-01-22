0ae34cac4092ce47552fbd78a43fe98bfcfcf125
Nolan Lawson <nolan@nolanlawson.com>
2023-01-22 09:27:55 -0800

    fix: better comment

--- bin/setup-benchmark.sh
+++ bin/setup-benchmark.sh
@@ -5,7 +5,8 @@ set -e
 cd ./test/benchmark
 
 # Tachometer doesn't seem to be able to locate relative files anywhere but the currect directory. So
-# move everything we need right here
+# move every file we need right here.
+# See also: https://github.com/google/tachometer/issues/244
 ln -sf ../../node_modules/emoji-picker-element-data/en/emojibase/data.json ./data.json
 
 for file in index.js picker.js database.js; do
