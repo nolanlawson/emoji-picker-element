5a8fc19b961ffbd02aa58a9b8cb41e0034f5aeda
Nolan Lawson <nolan@nolanlawson.com>
2023-01-22 13:09:04 -0800

    fix: preserve state if component disconnects then immediately reconnects (#313)
    Fixes #312

--- test/spec/picker/lifecycle.test.js
+++ test/spec/picker/lifecycle.test.js
@@ -2,6 +2,7 @@ import { basicAfterEach, basicBeforeEach, tick } from '../shared'
 import Picker from '../../../src/picker/PickerElement'
 import { getByRole, waitFor } from '@testing-library/dom'
 import { DEFAULT_DATA_SOURCE } from '../../../src/database/constants'
+import { openIndexedDBRequests } from '../../../src/database/databaseLifecycle.js'
 
 describe('lifecycle', () => {
   beforeEach(basicBeforeEach)
@@ -29,7 +30,8 @@ describe('lifecycle', () => {
     expect(fetch).toHaveBeenLastCalledWith(DEFAULT_DATA_SOURCE, { method: 'HEAD' })
 
     document.body.removeChild(picker)
-    await tick(20)
+    await tick(60)
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
   })
 
   test('database.close() is called when disconnected', async () => {
@@ -42,12 +44,13 @@ describe('lifecycle', () => {
     const spy = jest.spyOn(picker.database, 'close')
 
     document.body.removeChild(picker)
-    await tick(20)
+    await tick(60)
 
     expect(spy).toHaveBeenCalled()
     expect(spy).toHaveBeenCalledTimes(1)
 
     spy.mockRestore()
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
   })
 
   test('connect and immediately disconnect', async () => {
@@ -55,10 +58,25 @@ describe('lifecycle', () => {
     document.body.appendChild(picker)
     document.body.removeChild(picker)
 
-    await tick(20)
+    await tick(60)
 
     expect(fetch).toHaveBeenCalledTimes(1)
     expect(fetch).toHaveBeenLastCalledWith(DEFAULT_DATA_SOURCE, undefined)
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
+  })
+
+  test('connect and immediately disconnect twice', async () => {
+    const picker = new Picker()
+    document.body.appendChild(picker)
+    document.body.removeChild(picker)
+    document.body.appendChild(picker)
+    document.body.removeChild(picker)
+
+    await tick(60)
+
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(fetch).toHaveBeenLastCalledWith(DEFAULT_DATA_SOURCE, undefined)
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
   })
 
   test('connect, disconnect, and reconnect with a particular timing (#225)', async () => {
@@ -74,6 +92,85 @@ describe('lifecycle', () => {
 
     await tick(20)
     document.body.removeChild(picker)
+    await tick(60)
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
+  })
+
+  test('preserves state if component is disconnected and reconnected synchronously', async () => {
+    const picker = new Picker()
+    document.body.appendChild(picker)
     await tick(20)
+
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(fetch).toHaveBeenLastCalledWith(DEFAULT_DATA_SOURCE, undefined)
+    await expect(() => (
+      expect(getByRole(picker.shadowRoot, 'option', { name: /😀/ })).toBeVisible()
+    ))
+
+    document.body.removeChild(picker)
+    document.body.appendChild(picker)
+
+    await tick(20)
+
+    expect(fetch).toHaveBeenCalledTimes(1) // fetch is not called again because no re-render
+    await expect(() => (
+      expect(getByRole(picker.shadowRoot, 'option', { name: /😀/ })).toBeVisible()
+    ))
+
+    await tick(20)
+    document.body.removeChild(picker)
+    await tick(60)
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
+  })
+
+  test('does not preserve state if component is disconnected and reconnected in separate microtasks', async () => {
+    const picker = new Picker()
+    document.body.appendChild(picker)
+    await tick(20)
+
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(fetch).toHaveBeenLastCalledWith(DEFAULT_DATA_SOURCE, undefined)
+    await expect(() => (
+      expect(getByRole(picker.shadowRoot, 'option', { name: /😀/ })).toBeVisible()
+    ))
+
+    document.body.removeChild(picker)
+    await Promise.resolve()
+    document.body.appendChild(picker)
+
+    await tick(20)
+
+    expect(fetch).toHaveBeenCalledTimes(2) // fetch is called again due to re-render
+    expect(fetch).toHaveBeenLastCalledWith(DEFAULT_DATA_SOURCE, { method: 'HEAD' }) // cached, so does a HEAD
+    await expect(() => (
+      expect(getByRole(picker.shadowRoot, 'option', { name: /😀/ })).toBeVisible()
+    ))
+
+    await tick(20)
+    document.body.removeChild(picker)
+    await tick(60)
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
+  })
+
+  test('connect and immediately disconnect twice, then immediately reconnect', async () => {
+    const picker = new Picker()
+    document.body.appendChild(picker)
+    document.body.removeChild(picker)
+    document.body.appendChild(picker)
+    document.body.removeChild(picker)
+    document.body.appendChild(picker)
+
+    await tick(20)
+
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(fetch).toHaveBeenLastCalledWith(DEFAULT_DATA_SOURCE, undefined)
+    await expect(() => (
+      expect(getByRole(picker.shadowRoot, 'option', { name: /😀/ })).toBeVisible()
+    ))
+
+    await tick(20)
+    document.body.removeChild(picker)
+    await tick(60)
+    expect(Object.keys(openIndexedDBRequests).length).toBe(0) // no open IDB connections
   })
 })
