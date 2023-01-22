2b849cca83416967ac17c68c0a09faaf08b5dc8a
Nolan Lawson <nolan@nolanlawson.com>
2023-01-22 10:23:47 -0800

    fix: lint

--- config/babelJestTransform.cjs
+++ config/babelJestTransform.cjs
@@ -3,6 +3,6 @@ const babelJest = require('babel-jest').default
 module.exports = babelJest.createTransformer({
   presets: [],
   plugins: [
-    "@babel/plugin-transform-modules-commonjs"
+    '@babel/plugin-transform-modules-commonjs'
   ]
 })
