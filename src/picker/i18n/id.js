98a07daedb05349c489a780577ea409d7e194888
Matt Toschlog <matt@toschlog.com>
2023-02-22 12:18:41 -0500

    Added translations for Hindi & Indonesian.

---
+++ src/picker/i18n/id.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'Kategori',
+  emojiUnsupportedMessage: 'Browser Anda tidak mendukung emoji warna.',
+  favoritesLabel: 'Favorit',
+  loadingMessage: 'Memuat...',
+  networkErrorMessage: 'Tidak dapat memuat emoji.',
+  regionLabel: 'Pemilih emoji',
+  searchDescription: 'Ketika hasil pencarian tersedia, tekan atas atau bawah untuk menyeleksi dan enter untuk memilih.',
+  searchLabel: 'Cari',
+  searchResultsLabel: 'Hasil Pencarian',
+  skinToneDescription: 'Saat diperluas tekan atas atau bawah untuk menyeleksi dan enter untuk memilih.',
+  skinToneLabel: 'Pilih warna skin (saat ini {skinTone})',
+  skinTonesLabel: 'Warna skin',
+  skinTones: [
+    'Default',
+    'Light',
+    'Medium light',
+    'Medium',
+    'Medium dark',
+    'Dark'
+  ],
+  categories: {
+    custom: 'Kustom',
+    'smileys-emotion': 'Smiley dan emoticon',
+    'people-body': 'Orang dan bagian tubuh',
+    'animals-nature': 'Hewan dan tumbuhan',
+    'food-drink': 'Makanan dan minuman',
+    'travel-places': 'Rekreasi dan tempat',
+    activities: 'Aktivitas',
+    objects: 'Objek',
+    symbols: 'Simbol',
+    flags: 'Bendera'
+  }
+}
