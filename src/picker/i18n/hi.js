db7021aaadb7d47aa8f148418aa9c228bf08b6ba
toschlog <matt@toschlog.com>
2023-02-25 11:14:46 -0500

    feat: added Hindi & Indonesian text (#322)

---
+++ src/picker/i18n/hi.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'श्रेणियाँ',
+  emojiUnsupportedMessage: 'आपका ब्राउज़र कलर इमोजी का समर्थन नहीं करता।',
+  favoritesLabel: 'पसंदीदा',
+  loadingMessage: 'लोड हो रहा है...',
+  networkErrorMessage: 'इमोजी लोड नहीं हो सके।',
+  regionLabel: 'इमोजी चुननेवाला',
+  searchDescription: 'जब खोज परिणाम उपलब्ध हों तो चयन करने के लिए ऊपर या नीचे दबाएं और चुनने के लिए एंटर दबाएं।',
+  searchLabel: 'खोज',
+  searchResultsLabel: 'खोज के परिणाम',
+  skinToneDescription: 'जब विस्तृत किया जाता है तो चयन करने के लिए ऊपर या नीचे दबाएं और चुनने के लिए एंटर दबाएं।',
+  skinToneLabel: 'त्वचा का रंग चुनें (वर्तमान में {skinTone})',
+  skinTonesLabel: 'त्वचा के रंग',
+  skinTones: [
+    'डिफॉल्ट',
+    'हल्का',
+    'मध्यम हल्का',
+    'मध्यम',
+    'मध्यम गहरा',
+    'गहरा'
+  ],
+  categories: {
+    custom: 'कस्टम',
+    'smileys-emotion': 'स्माइली और इमोटिकॉन्स',
+    'people-body': 'लोग और शरीर',
+    'animals-nature': 'पशु और प्रकृति',
+    'food-drink': 'खाद्य और पेय',
+    'travel-places': 'यात्रा और स्थान',
+    activities: 'गतिविधियां',
+    objects: 'वस्तुएं',
+    symbols: 'प्रतीक',
+    flags: 'झंडे'
+  }
+}
