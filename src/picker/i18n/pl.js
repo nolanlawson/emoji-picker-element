69b2f81e551a74da52dc0030e2db8880cd4ff766
Michał Orman <michal.orman@gmail.com>
2023-04-18 04:45:39 +0200

    feat: added polish translation file (#325)

---
+++ src/picker/i18n/pl.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'Kategorie',
+  emojiUnsupportedMessage: 'Twoja przeglądarka nie wspiera kolorowych emotikon.',
+  favoritesLabel: 'Ulubione',
+  loadingMessage: 'Ładuję…',
+  networkErrorMessage: 'Nie można załadować emoji.',
+  regionLabel: 'Selektor emoji',
+  searchDescription: 'Kiedy wyniki wyszukiwania będą dostępne, wciśnij góra lub dół aby wybrać oraz enter aby zatwierdzić wybór.',
+  searchLabel: 'Wyszukaj',
+  searchResultsLabel: 'Wyniki wyszukiwania',
+  skinToneDescription: 'Po rozwinięciu wciśnij góra lub dół aby wybrać oraz enter aby zatwierdzić wybór.',
+  skinToneLabel: 'Wybierz odcień skóry (aktualnie {skinTone})',
+  skinTonesLabel: 'Odcienie skóry',
+  skinTones: [
+    'Domyślna',
+    'Jasna',
+    'Średnio-jasna',
+    'Średnia',
+    'Średnio-ciemna',
+    'Ciemna'
+  ],
+  categories: {
+    custom: 'Własne',
+    'smileys-emotion': 'Uśmiechy',
+    'people-body': 'Ludzie',
+    'animals-nature': 'Zwierzęta i natura',
+    'food-drink': 'Żywność i napoje',
+    'travel-places': 'Podróże i miejsca',
+    activities: 'Aktywności',
+    objects: 'Obiekty',
+    symbols: 'Symbole',
+    flags: 'Flagi'
+  }
+}
