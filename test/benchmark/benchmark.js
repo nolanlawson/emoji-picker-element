563afe4ad2705ac804f506943b906c77c14a1649
Nolan Lawson <nolan@nolanlawson.com>
2023-12-10 17:15:05 -0800

    perf: add more benchmarks

--- test/benchmark/benchmark.js
+++ test/benchmark/benchmark.js
@@ -1,31 +1,43 @@
-// hijack the performance.mark/measure API so we can add our own marks/measures just for the benchmark
-const { mark, measure } = performance
+function instrumentPickerLoading() {
+  // hijack the performance.measure API so we can add our own measures just for the benchmark
+  const { measure } = performance
 
-performance.mark = function (name) {
-  if (name === 'initialLoad') {
-    mark.call(performance, 'benchmark-start')
-  }
-}
-
-performance.measure = function (name, start) {
-  if (name === 'initialLoad' && start === 'initialLoad') {
-    // test to make sure the picker loaded with no errors
-    const hasErrors = document.querySelector('emoji-picker') && document.querySelector('emoji-picker')
-      .shadowRoot.querySelector('.message:not(.gone)')
-    if (hasErrors) {
-      console.error('picker is showing an error message')
-    } else {
-      measure.call(performance, 'benchmark-total', 'benchmark-start')
+  performance.measure = function overriddenMeasure(name, start) {
+    if (name === 'initialLoad' && start === 'initialLoad') {
+      // test to make sure the picker loaded with no errors
+      const hasErrors = document.querySelector('emoji-picker') && document.querySelector('emoji-picker')
+        .shadowRoot.querySelector('.message:not(.gone)')
+      if (hasErrors) {
+        console.error('picker is showing an error message')
+      } else {
+        measure.call(performance, 'benchmark-total', 'initialLoad')
+      }
     }
   }
 }
 
-// Fake an eTag on the headers for the emoji-picker data so that we actually reuse the cache.
-// Tachometer doesn't serve an eTag by default
-const nativeGet = Headers.prototype.get
-Headers.prototype.get = function (name) {
-  if (name.toLowerCase() === 'etag') {
-    return 'W/fakeEtag'
+function useFakeEtag() {
+  // Fake an eTag on the headers for the emoji-picker data so that we actually reuse the cache.
+  // Tachometer doesn't serve an eTag by default
+  const nativeGet = Headers.prototype.get
+  Headers.prototype.get = function (name) {
+    if (name.toLowerCase() === 'etag') {
+      return 'W/fakeEtag'
+    }
+    return nativeGet.call(this, name)
   }
-  return nativeGet.call(this, name)
+}
+
+const params = new URLSearchParams(location.search)
+const benchmark = params.get('benchmark') || 'first-load'
+
+if (benchmark === 'first-load') {
+  instrumentPickerLoading()
+  await import('./first-load.benchmark.js')
+} else if (benchmark === 'second-load') {
+  instrumentPickerLoading()
+  useFakeEtag()
+  await import('./second-load.benchmark.js')
+} else {
+  await import('./database-interactions.benchmark.js')
 }
---
+++ test/benchmark/first-load.benchmark.js
@@ -0,0 +1,5 @@
+import Picker from './picker.js'
+
+const dataSource = './data.json'
+
+document.body.appendChild(new Picker({ dataSource }))
