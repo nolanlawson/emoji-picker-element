7a135f33684acfc4eeea656ace232f731906ae30
Nolan Lawson <nolan@nolanlawson.com>
2024-05-25 09:52:04 -0700

    chore: update dependencies (#422)

--- bin/setup-benchmark.sh
+++ bin/setup-benchmark.sh
@@ -4,7 +4,7 @@ set -e
 
 cd ./test/benchmark
 
-# Tachometer doesn't seem to be able to locate relative files anywhere but the currect directory. So
+# Tachometer doesn't seem to be able to locate relative files anywhere but the current directory. So
 # move every file we need right here.
 # See also: https://github.com/google/tachometer/issues/244
 ln -sf ../../node_modules/emoji-picker-element-data/en/emojibase/data.json ./data.json
