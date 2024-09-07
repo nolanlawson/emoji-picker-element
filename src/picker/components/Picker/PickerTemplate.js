1eb21b706f4dfeac754181abd9310c6c462e7bbe
Nolan Lawson <nolan@nolanlawson.com>
2024-09-07 09:13:14 -0700

    refactor: rename

--- src/picker/components/Picker/PickerTemplate.js
+++ src/picker/components/Picker/PickerTemplate.js
@@ -191,7 +191,7 @@ export function render (container, state, helpers, events, actions, refs, abortS
               Improve performance in custom emoji by using \`content-visibility: auto\` on every category 
               The \`--expected-num-rows\` is also used in these calculations to contain the intrinsic height
             -->
-          <div class="emoji-menu ${!state.searchMode && emojiWithCategory.category ? 'lazy' : ''}"
+          <div class="emoji-menu ${!state.searchMode && emojiWithCategory.category ? 'hide-offscreen' : ''}"
                style=${`--expected-num-rows: ${Math.ceil(emojiWithCategory.emojis.length / state.numColumns)}`}
                role="${state.searchMode ? 'listbox' : 'menu'}"
                aria-labelledby="menu-label-${i}"
