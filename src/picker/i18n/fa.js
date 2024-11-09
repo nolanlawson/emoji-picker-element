3be211f9cd3591018e48bc78135da9136a61e301
Ali Faraji <31096694+alifa98@users.noreply.github.com>
2024-11-09 01:12:09 -0500

    Added Persian Language in i18n

---
+++ src/picker/i18n/fa.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'دسته‌بندی‌ها',
+  emojiUnsupportedMessage: 'مرورگر شما از ایموجی‌های رنگی پشتیبانی نمی‌کند.',
+  favoritesLabel: 'محبوب‌ها',
+  loadingMessage: 'در حال بارگذاری…',
+  networkErrorMessage: 'بارگذاری ایموجی‌ها با مشکل مواجه شد.',
+  regionLabel: 'انتخاب‌کننده ایموجی',
+  searchDescription: 'در صورت موجود بودن نتایج جستجو، با کلیدهای بالا یا پایین برای انتخاب و اینتر برای تأیید استفاده کنید.',
+  searchLabel: 'جستجو',
+  searchResultsLabel: 'نتایج جستجو',
+  skinToneDescription: 'در حالت باز شده، با کلیدهای بالا یا پایین انتخاب کرده و با اینتر تأیید کنید.',
+  skinToneLabel: 'انتخاب رنگ پوست (در حال حاضر {skinTone})',
+  skinTonesLabel: 'رنگ‌های پوست',
+  skinTones: [
+    'پیش‌فرض',
+    'روشن',
+    'روشن متوسط',
+    'متوسط',
+    'تیره متوسط',
+    'تیره'
+  ],
+  categories: {
+    custom: 'دلخواه',
+    'smileys-emotion': 'لبخندها و احساسات',
+    'people-body': 'افراد و بدن',
+    'animals-nature': 'حیوانات و طبیعت',
+    'food-drink': 'غذا و نوشیدنی',
+    'travel-places': 'سفر و مکان‌ها',
+    activities: 'فعالیت‌ها',
+    objects: 'اشیاء',
+    symbols: 'نمادها',
+    flags: 'پرچم‌ها'
+  }
+}
