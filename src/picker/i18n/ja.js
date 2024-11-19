ddce8267beb3817131c33445cd1edf64a533be98
Camille Drapier <camille.drapier@gmail.com>
2024-11-19 19:26:06 +0900

    feat: add Japanese language support in i18n

---
+++ src/picker/i18n/ja.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'カテゴリー',
+  emojiUnsupportedMessage: 'このブラウザではカラー絵文字を表示できません。',
+  favoritesLabel: 'お気に入り',
+  loadingMessage: '読込中…',
+  networkErrorMessage: '絵文字を読み込めませんでした。',
+  regionLabel: '絵文字を追加',
+  searchDescription: '検索結果を矢印キーで選択し、エンターキーで決定できます。',
+  searchLabel: '検索',
+  searchResultsLabel: '検索結果',
+  skinToneDescription: 'メニューを開いて矢印キーで選択し、エンターキーで決定できます。',
+  skinToneLabel: '肌の色を選択 (選択中の色: {skinTone})',
+  skinTonesLabel: '肌の色',
+  skinTones: [
+    'デフォルト',
+    '明るい',
+    'やや明るい',
+    '中間',
+    'やや暗い',
+    '暗い'
+  ],
+  categories: {
+    custom: 'カスタム',
+    'smileys-emotion': '顔文字',
+    'people-body': '人・身体',
+    'animals-nature': '動物・自然',
+    'food-drink': '食べ物・飲み物',
+    'travel-places': '旅行・場所',
+    activities: 'アクティビティ',
+    objects: 'オブジェクト',
+    symbols: '記号',
+    flags: '旗'
+  }
+}
