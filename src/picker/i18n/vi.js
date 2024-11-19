c2ffb53e7f47817762934bde10263827dd8b5823
Nguyễn Đinh Hoàng Anh <73628269+ZoloPro@users.noreply.github.com>
2024-11-19 12:12:53 +0700

    feat: add Vietnamese language support in i18n (#474)
    Co-authored-by: Nolan Lawson <nolan@nolanlawson.com>

---
+++ src/picker/i18n/vi.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'Danh mục',
+  emojiUnsupportedMessage: 'Trình duyệt của bạn không hỗ trợ emoji màu.',
+  favoritesLabel: 'Yêu thích',
+  loadingMessage: 'Đang tải…',
+  networkErrorMessage: 'Không thể tải emoji.',
+  regionLabel: 'Chọn emoji',
+  searchDescription: 'Khi có kết quả tìm kiếm, nhấn lên hoặc xuống để chọn và nhấn Enter để chọn.',
+  searchLabel: 'Tìm kiếm',
+  searchResultsLabel: 'Kết quả tìm kiếm',
+  skinToneDescription: 'Khi mở rộng, nhấn lên hoặc xuống để chọn và nhấn Enter để chọn.',
+  skinToneLabel: 'Chọn tông màu da (hiện tại {skinTone})',
+  skinTonesLabel: 'Tông màu da',
+  skinTones: [
+    'Mặc định',
+    'Sáng',
+    'Sáng vừa',
+    'Trung bình',
+    'Tối vừa',
+    'Tối'
+  ],
+  categories: {
+    custom: 'Tùy chỉnh',
+    'smileys-emotion': 'Mặt cười và cảm xúc',
+    'people-body': 'Người và cơ thể',
+    'animals-nature': 'Động vật và thiên nhiên',
+    'food-drink': 'Thức ăn và đồ uống',
+    'travel-places': 'Du lịch và địa điểm',
+    activities: 'Hoạt động',
+    objects: 'Đồ vật',
+    symbols: 'Biểu tượng',
+    flags: 'Cờ'
+  }
+}
