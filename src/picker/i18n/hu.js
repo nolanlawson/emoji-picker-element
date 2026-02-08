c23028f44412465b1a66f948f3a59d47112288a4
hackedhorizon <raczdwork@proton.me>
2026-02-08 22:55:19 +0100

    feat: add Hungarian translations (#513)
    * i18n (hungarian translations)
    
    Add hungarian translations.
    
    * fix syntax
    
    ---------
    
    Co-authored-by: Nolan Lawson <nolan@nolanlawson.com>

---
+++ src/picker/i18n/hu.js
@@ -0,0 +1,34 @@
+export default {
+  categoriesLabel: 'Kategóriák',
+  emojiUnsupportedMessage: 'A böngésző nem támogatja a színes emojikat.',
+  favoritesLabel: 'Kedvencek',
+  loadingMessage: 'Betöltés…',
+  networkErrorMessage: 'Nem sikerült betölteni az emojikat.',
+  regionLabel: 'Emoji választó',
+  searchDescription: 'Ha a keresési eredmények elérhetők, nyomja meg a fel vagy le nyilat a kiválasztáshoz és az entert a választáshoz.',
+  searchLabel: 'Keresés',
+  searchResultsLabel: 'Keresési eredmények',
+  skinToneDescription: 'Kibontva a fel vagy le nyíl megnyomásával választhat és az enterrel erősítheti meg.',
+  skinToneLabel: 'Válasszon bőrtónust (jelenleg {skinTone})',
+  skinTonesLabel: 'Bőrtónus',
+  skinTones: [
+    'Alapértelmezett',
+    'Világos',
+    'Közepesen világos',
+    'Közepes',
+    'Közepesen sötét',
+    'Sötét'
+  ],
+  categories: {
+    custom: 'Egyéni',
+    'smileys-emotion': 'Hangulatjelek és érzelmek',
+    'people-body': 'Emberek és test',
+    'animals-nature': 'Állatok és természet',
+    'food-drink': 'Ételek és italok',
+    'travel-places': 'Utazás és helyek',
+    activities: 'Tevékenységek',
+    objects: 'Tárgyak',
+    symbols: 'Szimbólumok',
+    flags: 'Zászlók'
+  }
+}
